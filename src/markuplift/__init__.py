cf5669e932b656a6e6e04b2a999dbceac8b19334
Robert Smallshire <robert@smallshire.org.uk>
2025-09-24 17:24:09 +0200

    Bump version: 1.0.0 → 2.0.0

--- src/markuplift/__init__.py
+++ src/markuplift/__init__.py
@@ -31,5 +31,5 @@ __all__ = [
 from collections import namedtuple
 
 Version = namedtuple("Version", ["major", "minor", "patch"])
-__version__ = "1.0.0"
+__version__ = "2.0.0"
 __version_info__ = Version(*(__version__.split(".")))
